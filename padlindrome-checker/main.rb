1f627a9db649b7ffea2e2d8c76ce380f475d774c
michaelsam <137851066+msam4@users.noreply.github.com>
2024-07-25 12:11:17 +0900

    Add an easier method checker using ruby method reverse

--- padlindrome-checker/main.rb
+++ padlindrome-checker/main.rb
@@ -1,10 +1,14 @@
-def is_palidrome(word)
+def is_palindrome_easy(word)
+  word == word.reverse
+end
+
+def is_palindrome(word)
   return true if word.length <= 1
   return false if word[0] != word[-1]
-  is_palidrome(word[1, word.length-2])
+  is_palindrome(word[1, word.length-2])
 end
 
 puts "Padindrome Word Checker"
 print "Word: "
 word = gets.chomp
-puts "Is Padindrome: #{is_palidrome(word)}"
+puts "Is Padindrome: #{is_palindrome_easy(word)}"
