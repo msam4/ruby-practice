7ae3c069b13e268534e029d5f433c064198034f6
michaelsam <137851066+msam4@users.noreply.github.com>
2024-07-27 16:16:15 +0900

    Create simple search file

---
+++ file-searcher/main.rb
@@ -0,0 +1,38 @@
+def search(key)
+  result = {}
+  Dir.glob("*.txt") do |filename|
+    f = File.open(filename)
+    f.each_with_index do |line, index|
+      if line.include?(key)
+        result[index + 1] = filename
+      end
+    end
+    f.close
+  end
+  result
+end
+
+def find(key)
+  result = {}
+  Dir.glob("*.txt") do |filename|
+    File.foreach(filename).each_with_index do |line, index|
+      if line.include?(key)
+        result[index + 1] = filename
+      end
+    end
+  end
+  result
+end
+
+puts "Search in Files"
+print "Key: "
+key = gets.chomp
+result = find(key)
+
+if result.length > 0
+  for key, value in result
+    puts "Filename: #{value} Line Number: #{key}"
+  end
+else
+  puts "Not Found"
+end
