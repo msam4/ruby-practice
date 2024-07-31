bc4857dd70f861633c08c840e3ee1ed075e772dc
michaelsam <137851066+msam4@users.noreply.github.com>
2024-07-31 11:50:29 +0900

    Create simple mad lib game

---
+++ mad-libs-game/main.rb
@@ -0,0 +1,15 @@
+puts "Mad Libs".center(50, "-")
+
+print "Enter a color: "
+color = gets.chomp
+print "Enter a plural noun: "
+plural_noun = gets.chomp
+print "Enter a celebrity's name: "
+celebrity = gets.chomp
+
+
+puts "---------------------------------------"
+puts "Roses are #{color}"
+puts "#{plural_noun} are blue"
+puts "I love #{celebrity}"
+puts "---------------------------------------"
