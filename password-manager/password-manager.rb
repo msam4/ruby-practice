86147ca9c4fb5e33833f027900aa576a926b7d21
Michael Sam <sam.michael.h@gmail.com>
2025-12-10 20:38:57 +0900

    Refactor code. Add a display for new creds after being inputed

--- password-manager/password-manager.rb
+++ password-manager/password-manager.rb
@@ -2,56 +2,115 @@ EMAIL = "me@example.com"
 PASSWORD = "example"
 PASSWORD_VAULT = { aws: {username: "sam", password: "password"} }
 
-puts "Welcome to the Password Manager!"
-puts "Please sign in your email."
-print "Enter email: "
-
-user_email = gets.chomp
-
-if user_email == EMAIL
-  print "Enter password: "
-  user_password = gets.chomp
-else
-  puts "Invalid email."
-  exit
+def welcome_message
+  puts "Welcome to the Password Manager!"
 end
 
-unless user_password != PASSWORD
+def prompt_user_for_email
+  puts "Please sign in your email."
+  print "Enter email: "
+  gets.chomp
+end
+
+def verify_user_email (user_email)
+  if user_email != EMAIL
+    puts "Invalid email."
+    exit
+  end
+end
+
+def prompt_user_for_password
+  print "Enter password: "
+  gets.chomp
+end
+
+def verify_user_password(user_password)
+  if user_password != PASSWORD
+    puts "Invalid password"
+    exit
+  end
+end
+
+def greet_user(user_email)
   puts "Hello, #{user_email}!"
   puts "What would you like to do?"
+end
+
+def menu_options
   puts "1. Add new credentials."
   puts "2. Retrieve an existing credentials."
   puts "3. Exit"
-  user_selection = gets.chomp
-else
-  puts "Invalid password."
-  exit
 end
 
-case user_selection
-when "1"
+def get_user_menu_selection
+  gets.chomp
+end
+
+def handle_user_selection(user_selection)
+  case user_selection
+  when "1"
+    new_service = set_new_service_name
+    set_username_for(new_service)
+    set_password_for(new_service)
+  when "2"
+    requested_service_name = retrieve_service_name
+    credentials = retrieve_service_credentials_for(requested_service_name)
+    display_service_credentials(requested_service_name, credentials)
+  else
+    exit_program
+  end
+end
+
+def set_new_service_name
   print "Enter new service credentials: "
   new_service = gets.chomp
-  PASSWORD_VAULT[new_service] = {}
+  PASSWORD_VAULT[new_service.to_sym] = {}
+  new_service
+end
 
+def set_username_for(service)
   print "Please enter the username for this service: "
   new_service_username = gets.chomp
-  PASSWORD_VAULT[new_service]["username"] = new_service_username
+  PASSWORD_VAULT[service.to_sym][:username] = new_service_username
+end
 
+def set_password_for(service)
   print "Please enter the password for this service: "
   new_service_password = gets.chomp
-  PASSWORD_VAULT[new_service]["password"] = new_service_password
+  PASSWORD_VAULT[service.to_sym][:password] = new_service_password
+  # puts the new service for users here PASSWORD_VAULT[service.to_sym][:username] and [:password]
+end
 
-  puts PASSWORD_VAULT
-when "2"
+def retrieve_service_name
   print "Please enter the name of the service you wish to access credentials for: "
-  requested_service_name = gets.chomp
-  credentials = PASSWORD_VAULT[requested_service_name.to_sym]
-  puts "Here are the credentials for #{requested_service_name}:"
+  gets.chomp
+end
+
+def retrieve_service_credentials_for(service_name)
+  PASSWORD_VAULT[service_name.to_sym]
+end
+
+def display_service_credentials(service_name, credentials)
+  puts "Here are the credentials for #{service_name}:"
   credentials.each do |key, val|
     puts "#{key}: #{val}"
   end
-else
+end
+
+def exit_program
   puts "Exiting manager. Goodbye!"
   exit
+end
+
+welcome_message
+user_email = prompt_user_for_email
+verify_user_email(user_email)
+user_password = prompt_user_for_password
+verify_user_password(user_password)
+greet_user(user_email)
+
+loop do
+  menu_options
+  user_selection = get_user_menu_selection
+  handle_user_selection(user_selection)
 end
